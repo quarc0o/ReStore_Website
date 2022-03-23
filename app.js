01fa8d36b1b5838f0e6cb42d1f8c78a53bc4ce7e
Emmanuel <emmanuelq150@gmail.com>
2022-03-23 20:47:32 +0100

    Began work on new accordion

--- app.js
+++ app.js
@@ -23,4 +23,14 @@ function setClass(els, className, fnName) {
     }
 }
 
+const accordion = document.getElementsByClassName('container');
+
+for (var i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+  });
+}
+
 });
+
+
